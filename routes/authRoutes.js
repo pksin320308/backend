2eeefd2906a0a7e6566ffa83490d2c1499dec013
pksin320308 <pksin320308@gmail.com>
2026-05-31 21:33:05 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,7 @@ const {
 
 const protect = require("../middleware/authMiddleware");
 const studentOnly = require("../middleware/studentMiddleware");
+const adminOnly = require("../middleware/adminMiddleware");
 
 const router = express.Router();
 
@@ -19,11 +20,13 @@ router.get("/captcha", getCaptcha);
 router.post("/register", registerStudent);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/profile", protect, profile);
+router.get("/admin/profile", protect("admin"), adminOnly, profile);
+
+router.get("/student/profile", protect("student"), studentOnly, profile);
 
 //course
 
-router.post("/", protect, studentOnly, requestCourse);
-router.get("/my-requests", protect, studentOnly, getMyCourseRequests);
+router.post("/", protect("student"), studentOnly, requestCourse);
+router.get("/my-requests", protect("student"), studentOnly, getMyCourseRequests);
 
 module.exports = router;
